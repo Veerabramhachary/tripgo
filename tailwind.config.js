b549a504f7089343870cc3f83b82a9a2fd35afe8
Veera Bramha Chary <veerabramhachary2003@gmail.com>
2026-09-21 12:01:13 +0530

    chore: update Tailwind CSS configuration with content paths and presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  // Make sure your content paths match your project structure
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")], // <-- Add this line
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
